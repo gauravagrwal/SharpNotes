69cb7106893cf0962f55da99d5293dd22a2eef47
Gaurav Agrawal <gauravag.1005@gmail.com>
2022-07-02 18:02:11 +0530

    app crash fix

--- SharpNotes/SharpNotes/Views/NoteDetailsPage.xaml.cs
+++ SharpNotes/SharpNotes/Views/NoteDetailsPage.xaml.cs
@@ -1,9 +1,22 @@
-﻿using Xamarin.Forms;
+﻿using SharpNotes.ViewModels;
+using Xamarin.Forms;
 
 namespace SharpNotes.Views
 {
     public partial class NoteDetailsPage : ContentPage
     {
         public NoteDetailsPage() => InitializeComponent();
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            (BindingContext as NoteDetailsViewModel)?.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            (BindingContext as NoteDetailsViewModel)?.OnDisappearing();
+        }
     }
 }
